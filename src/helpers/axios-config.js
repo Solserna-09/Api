6ddfc96a17b0c492346c49c1b7c7e91a51334c09
Solserna-09 <93735291+Solserna-09@users.noreply.github.com>
2023-04-11 22:36:28 -0500

    Update axios-config.js
    Url

--- src/helpers/axios-config.js
+++ src/helpers/axios-config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL:'https://apimongo24.herokuapp.com/api/'
+    baseURL:'https://my-backend-api.herokuapp.com/api/'
 
 });
 
